67b1de343382a3506c6b92fe5bb0bcd65e921e56
ManN <dubkois@gmail.com>
2019-08-07 17:00:51 +0200

    .

---
+++ scripts/make_summary.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+usage(){
+  echo "Usage: $0 <result-folder>"
+}
+
+echo $@
+if [ $# -ne 1 ]
+then
+  usage
+  exit 10
+fi
+
+if [ ! -d $1 ]
+then
+  usage
+  exit 1
+fi
+
+agg=""
+if [ ! -f $1/global.dat ]
+then
+  # run folder > Agreggate first
+  agg="yes"
+  for t in global topology temperature hygrometry; do rm -v $1/$t.dat; done
+  for e in $1/e*_r
+  do
+    for t in global topology temperature hygrometry; do cat $e/$t.dat >> $1/$t.dat; done
+  done
+fi
+
+./scripts/plant_dynamics.sh -q -f $1/global.dat -c "Plants;Species:y2" -o $1/dynamics_summary.png
+
+if [ -z "$agg" ]
+then
+  ./scripts/controller_to_pdf.sh $1/controller.tex
+  dot -Tpng $1/controller.dot -o $1/controller_dot.png
+fi
+
+for t in topology temperature hygrometry
+do
+  ./scripts/plot_voxels.sh $1/$t.dat
+done
+
+if [ -z "$agg" ]
+then
+  montage -tile 3x2 $1/controller_tex.png $1/dynamics_summary.png $1/controller_dot.png \
+                    $1/topology.png $1/temperature.png $1/hygrometry.png\
+          -geometry +2+2 $1/summary.png
+else
+  montage -tile 3x2 $1/dynamics_summary.png $1/topology.png $1/temperature.png $1/hygrometry.png -geometry +2+2 $1/summary.png
+fi
+# xdg-open $1/summary.png
+
